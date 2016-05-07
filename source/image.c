fdf0e3370c0355da73921a01e142570c7e4e1ae1
Carlos García <carlosgarciafm@gmail.com>
2016-05-07 16:52:05 -0500

    Modify the condition to evaluate for an easier one to read.

--- source/image.c
+++ source/image.c
@@ -7,7 +7,7 @@ GS* verticalPattern(unsigned int height, unsigned int width) {
 	GS* INPUT = newBMP(height, width);
 	for(n = 0; n < INPUT->size; n++)
 		/* Sets all the pixels on the left side of the image to 255; the rest are set to 0. */
-		INPUT->pixel[n] = ((n / (INPUT->width / 2)) % 2 == 0)? 255: 0;
+		INPUT->pixel[n] = ((n % INPUT->width) < (INPUT->width / 2))? 255: 0;
 	return INPUT;
 }
 
